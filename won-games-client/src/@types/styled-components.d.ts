ea8aee2fb2f4808e3b75d5a5135c2668bd1c115c
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-14 14:37:00 -0300

    add themes

---
+++ won-games-client/src/@types/styled-components.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components'
+import theme from 'styles/theme'
+
+// inferência de tipos
+type Theme = typeof theme
+
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
