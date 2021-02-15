1b5077a7ba3ab297d4ce93921845bb22a80709ee
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-15 18:01:24 -0300

    create logo component and render helper

---
+++ won-games-client/src/components/Logo/test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+
+import { Logo } from '.'
+
+describe('<Logo />', () => {
+  it('should render a white label by default', () => {
+    //
+  })
+})
