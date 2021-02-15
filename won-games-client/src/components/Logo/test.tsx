2691daab89f4b813ac63cb922bdcf8f1b0774caf
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-15 18:43:29 -0300

    create Logo component and tests for logo

--- won-games-client/src/components/Logo/test.tsx
+++ won-games-client/src/components/Logo/test.tsx
@@ -1,9 +1,52 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import { Logo } from '.'
 
 describe('<Logo />', () => {
-  it('should render a white label by default', () => {
-    //
+  it('should be render a white label by default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should be render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('should be render a large logo when size large is passed', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should be render a normal logo when size is default or equal normal', () => {
+    renderWithTheme(<Logo size="normal" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should be render a large logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
