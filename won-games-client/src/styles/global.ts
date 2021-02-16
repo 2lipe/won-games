419e6dc925a9680c6ef8d1a3512a3debc9f129d2
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-16 01:00:09 -0300

    create MediaMatch helper and fix fonts

--- won-games-client/src/styles/global.ts
+++ won-games-client/src/styles/global.ts
@@ -1,13 +1,13 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-  @font-face {
+ @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 300;
     font-display: swap;
-    src: local('Poppins Light'), local('Poppins-Light')
-        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
   }
 
   @font-face {
@@ -15,8 +15,8 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: local('Poppins Regular'), local('Poppins-Regular')
-        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
   }
 
   @font-face {
@@ -24,8 +24,8 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal;
     font-weight: 600;
     font-display: swap;
-    src: local('Poppins SemiBold'), local('Poppins-SemiBold')
-        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
   }
 
   * {
