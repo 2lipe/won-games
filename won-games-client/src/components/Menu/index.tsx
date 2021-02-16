1d89d04f6eaacd7ccc30a71a5d3ca61d33f2c601
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-16 11:45:44 -0300

    init Menu component

---
+++ won-games-client/src/components/Menu/index.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+
+import { Logo } from 'components/Logo'
+
+import * as S from './styles'
+
+export const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClickOpenMenu = () => setIsOpen(true)
+
+  const handleClickCloseMenu = () => setIsOpen(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={handleClickOpenMenu}>
+        <MenuIcon aria-label="Open Menu" />
+      </S.IconWrapper>
+
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={handleClickCloseMenu} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
