53f2111a0d4141935faca174d11db76cf0f78672
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-16 22:01:28 -0300

    implementing lineColors and sizes to Heading

--- won-games-client/src/components/Heading/index.tsx
+++ won-games-client/src/components/Heading/index.tsx
@@ -2,20 +2,32 @@ import { ReactNode } from 'react'
 
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  size?: 'small' | 'medium'
+  lineColor?: LineColors
 }
 
 export const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium',
+  lineColor = 'primary'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    size={size}
+    lineColor={lineColor}
+  >
     {children}
   </S.Wrapper>
 )
--- won-games-client/src/components/Heading/styles.ts
+++ won-games-client/src/components/Heading/styles.ts
@@ -1,15 +1,27 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 
-import { HeadingProps } from '.'
+import { HeadingProps, LineColors } from '.'
 
 const WrapperModifiers = {
-  lineLeft: (theme: DefaultTheme) => css`
-    padding-left: ${theme.spacings.xsmall};
-    border-left: 0.7rem solid ${theme.colors.secondary};
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+
+    &::after {
+      width: 3rem;
+    }
   `,
 
-  lineBottom: (theme: DefaultTheme) => css`
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xxlarge};
+  `,
+
+  lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
+    padding-left: ${theme.spacings.xsmall};
+    border-left: 0.7rem solid ${theme.colors[lineColor]};
+  `,
+
+  lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
 
@@ -20,21 +32,19 @@ const WrapperModifiers = {
       content: '';
 
       width: 5rem;
-      border-bottom: 0.5rem solid ${theme.colors.primary};
+      border-bottom: 0.5rem solid ${theme.colors[lineColor]};
     }
   `
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom }) => css`
+  ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
     color: ${theme.colors[color!]};
     font-size: ${theme.font.sizes.xlarge};
 
-    ${media.greaterThan('medium')`
-      font-size: ${theme.font.sizes.xxlarge};
-    `};
+    ${lineLeft && WrapperModifiers.lineLeft(theme, lineColor!)};
+    ${lineBottom && WrapperModifiers.lineBottom(theme, lineColor!)};
 
-    ${lineLeft && WrapperModifiers.lineLeft(theme)};
-    ${lineBottom && WrapperModifiers.lineBottom(theme)};
+    ${!!size && WrapperModifiers[size](theme)};
   `}
 `
