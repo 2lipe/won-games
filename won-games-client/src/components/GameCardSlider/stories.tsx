b51df718a9f5541a0491d977c4b0d632f04cf238
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-02 15:48:39 -0300

    chore: init GameCardSlider

---
+++ won-games-client/src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,72 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { GameCardSlider, GameCardSliderProps } from '.'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  }
+]
+
+export default {
+  title: 'GameCardSlider',
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  },
+  component: GameCardSlider
+} as Meta
+
+export const Default: Story<GameCardSliderProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameCardSlider {...args} />
+  </div>
+)
