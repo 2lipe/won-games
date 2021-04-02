a999299f5f2c7108447e6bd5362d9d961d716234
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-02 16:11:40 -0300

    feat: create GameCardSlider with tests/storybook

--- won-games-client/src/components/GameCardSlider/test.tsx
+++ won-games-client/src/components/GameCardSlider/test.tsx
@@ -1,15 +1,62 @@
+import 'match-media-mock'
 import { render, screen } from '@testing-library/react'
 
 import { GameCardSlider } from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/population-zero-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  }
+]
 
 describe('<GameCardSlider />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameCardSlider />)
+  it('should be render 4 active cards', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
 
-    expect(
-      screen.getByRole('heading', { name: /GameCardSlider/i })
-    ).toBeInTheDocument()
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should be render white arrows if color is passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
   })
 })
