fdeab22b4bdd005830960acb57e6ab5b58fad166
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-02 18:01:27 -0300

    refact: change storybook styles

--- won-games-client/.storybook/preview.js
+++ won-games-client/.storybook/preview.js
@@ -7,6 +7,22 @@ import GlobalStyles from 'styles/global'
 
 addDecorator(withNextRouter())
 
+export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
