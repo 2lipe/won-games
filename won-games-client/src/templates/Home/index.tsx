ec018528d817f41aa4a6cb25ee56cd9967789bbc
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-03 10:09:55 -0300

    chore: Init Home Page

---
+++ won-games-client/src/templates/Home/index.tsx
@@ -0,0 +1,42 @@
+import { Container } from 'components/Container'
+import { Footer } from 'components/Footer'
+import { Heading } from 'components/Heading'
+import { Menu } from 'components/Menu'
+
+import * as S from './styles'
+
+export const Home = () => (
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        News
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Most Popular
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Upcomming
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="white">
+        Free Games
+      </Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </section>
+)
---
+++ won-games-client/src/templates/Home/test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Home } from '.'
+
+describe('<Home />', () => {
+  it('should be render menu and footer', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /contact us/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should be render the sections correctly', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Most Popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Free Games/i })
+    ).toBeInTheDocument()
+  })
+})
