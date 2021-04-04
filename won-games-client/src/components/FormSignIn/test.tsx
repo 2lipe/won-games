355742473232120812224f5712bed6decff69282
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-04 13:38:14 -0300

    chore: create tests for FormiSignIn

--- won-games-client/src/components/FormSignIn/test.tsx
+++ won-games-client/src/components/FormSignIn/test.tsx
@@ -1,13 +1,31 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import { FormSignIn } from '.'
 
 describe('<FormSignIn />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<FormSignIn />)
+  it('should be render the form', () => {
+    renderWithTheme(<FormSignIn />)
 
-    expect(screen.getByRole('heading', { name: /FormSignIn/i })).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should be render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password\?/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should be render text to sign up if already have an account', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+    expect(screen.getByText(/don't have an account\?/i)).toBeInTheDocument()
   })
 })
