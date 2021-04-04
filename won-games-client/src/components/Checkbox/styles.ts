cd3367f3a18a457d5b390d91483639fc85c4ddae
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-03 23:53:07 -0300

    feat: finalize checkbox styles

--- won-games-client/src/components/Checkbox/styles.ts
+++ won-games-client/src/components/Checkbox/styles.ts
@@ -1,12 +1,63 @@
 import styled, { css } from 'styled-components'
 import { CheckboxProps } from '.'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const Input = styled.input`
+  ${({ theme }) => css`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    appearance: none;
+    width: 1.8rem;
+    height: 1.8rem;
+    border: 0.2rem solid ${theme.colors.darkGray};
+    border-radius: 0.2rem;
+    position: relative;
+    outline: none;
+    transition: background border ${theme.transition.fast};
+
+    &:before {
+      content: '';
+      width: 0.6rem;
+      height: 0.9rem;
+      border: 0.2rem solid ${theme.colors.white};
+      border-top: 0;
+      border-left: 0;
+      transform: rotate(45deg);
+      position: absolute;
+      top: 0.1rem;
+      opacity: 0;
+      transition: ${theme.transition.fast};
+    }
+
+    &:focus {
+      box-shadow: 0 0 0.5rem ${theme.colors.primary};
+    }
+
+    &:checked {
+      border-color: ${theme.colors.primary};
+      background: ${theme.colors.primary};
+
+      &:before {
+        opacity: 1;
+      }
+    }
+  `}
+`
 
 type LabelColorProps = Pick<CheckboxProps, 'labelColor'>
 
 export const Label = styled.label<LabelColorProps>`
   ${({ theme, labelColor }) => css`
+    cursor: pointer;
+    padding-left: ${theme.spacings.xxsmall};
+    line-height: 1;
+
     color: ${theme.colors[labelColor!]};
   `}
 `
