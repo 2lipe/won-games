385fd51a98bf1bfe83814029fb35513368e07108
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-04 00:52:37 -0300

    feat: create Radio component

--- won-games-client/src/components/Radio/styles.ts
+++ won-games-client/src/components/Radio/styles.ts
@@ -1,3 +1,56 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main``
+import { RadioProps } from '.'
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const Input = styled.input`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    appearance: none;
+    width: 1.8rem;
+    height: 1.8rem;
+    border: 0.2rem solid ${theme.colors.primary};
+    border-radius: 50%;
+    background: transparent;
+    outline: none;
+    cursor: pointer;
+    transition: background ${theme.transition.fast};
+
+    &:focus {
+      box-shadow: 0 0 0.5rem ${theme.colors.primary};
+    }
+
+    &:before {
+      content: '';
+      width: 0.8rem;
+      height: 0.8rem;
+      border-radius: 50%;
+      background: ${theme.colors.primary};
+      transition: opacity ${theme.transition.fast};
+      opacity: 0;
+      position: absolute;
+    }
+
+    &:checked {
+      &:before {
+        opacity: 1;
+      }
+    }
+  `}
+`
+type LabelProps = Pick<RadioProps, 'labelColor'>
+
+export const Label = styled.label<LabelProps>`
+  ${({ theme, labelColor }) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    color: ${theme.colors[labelColor!]};
+    line-height: 1;
+    cursor: pointer;
+  `}
+`
